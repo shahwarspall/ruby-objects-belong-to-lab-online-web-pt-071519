b879e6ecfd5e2da011e5cd28d778d95d2528e1e8
Shahwar Spall <shevy.spall@gmail.com>
2019-08-14 03:09:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,11 @@
+requre 'pry'
 class Artist 
   attr_accessor :artist,:author
   
   def initialize(artist,author)
     @artist = artist
     @author = author
-    
+    blinding.pry
   end 
   
 end
